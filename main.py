c7b777ec4a4878e87586dab0addfda80f96ee555
arwynstorm88 <hounddog88@gmail.com>
2025-02-25 09:36:02 -0600

    formated output and added character list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import get_num_words
+#from stats import get_num_char
+from stats import generate_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,6 +11,11 @@ def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
     total_words = get_num_words(text)
-    print (f"{total_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print (f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    generate_report(text)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def get_num_words(text):
     words = text.split()
     wordcount = len(words)
     return wordcount
+def get_num_char(text):
+    num_char = {}
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            num_char[char] = num_char.get(char, 0) + 1
+    return num_char
+def sort_on(char_dict):
+    return char_dict["num"]
+def generate_report(text):
+    char_counts = get_num_char(text)
+    char_list = [{"char": char, "num": count} for char, count in char_counts.items()]
+    char_list.sort(reverse=True, key=sort_on)
+    for item in char_list:
+        print(f"{item['char']}: {item['num']}")
