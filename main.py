2937f215c65d1dd39ee095d919b75607252a6bea
arwynstorm88 <hounddog88@gmail.com>
2025-02-25 07:49:14 -0600

    moved word counter to seperate file

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-def num_words(texts):
-    words = texts.split()
-    wordcount = len(words)
-    return wordcount
+
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
-    total_words = num_words(text)
+    total_words = get_num_words(text)
     print (f"{total_words} words found in the document")
-    #print(text)
-
-
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    words = text.split()
+    wordcount = len(words)
+    return wordcount
