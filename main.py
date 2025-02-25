24fd0783c203ef37faddb0369f602f70d2030eb1
arwynstorm88 <hounddog88@gmail.com>
2025-02-25 10:18:31 -0600

    added user input for books available to be tested

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 from stats import get_num_words
-#from stats import get_num_char
 from stats import generate_report
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,11 +12,11 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
+    
+    text = get_book_text(sys.argv[1])
     total_words = get_num_words(text)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print (f"Found {total_words} total words")
     print("--------- Character Count -------")
